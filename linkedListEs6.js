b5e72a7818d48aae42cea59d685b8f8c5ba4fbed
Kaley Kriminger <kaley.kriminger@gmail.com>
2018-02-04 17:52:28 -0800

    improve readability and note clarity

--- linkedListEs6.js
+++ linkedListEs6.js
@@ -3,7 +3,7 @@
 
 // Linked list time complexity.
 // Same as stacks and queues.
-// //Linked list time complexity
+// Linked list time complexity
 // (Algorithm : Average : Worst-Case)
 // Search : O(n) : O(n)
 // Insert : O(1) : O(1)
@@ -25,51 +25,51 @@ class LinkedList{
   add(data){
     const nodeToAdd = new Node(data);
     let nodeToCheck = this.head;
-    if(!nodeToCheck){
-      //if there is no head, the head will be the new node.
+    if ( !nodeToCheck ) {
+      //if there is no head, the new node will become the head.
       this.head = nodeToAdd;
       this.length++;
       return nodeToAdd;
     }
-    //if the head has a node after it, iterate to the next node and check it, until you find a node w/o a next.
-    while(nodeToCheck.next){
+    //if there already is a head, iterate to the next node and check it, until you find a node w/o a next.
+    while ( nodeToCheck.next ) {
       nodeToCheck = nodeToCheck.next;
     }
-    //then you make the new node the tail node's next
+    //then you make the new node the tail node's next -- the new node becomes the tail.
     nodeToCheck.next = nodeToAdd;
     this.length++;
     return nodeToAdd;
   }
 
-  get(num){
+  get (num) {
     let nodeToCheck = this.head;
     let count = 0;
 
-    if(num > this.length) return "Doesn't Exist!"
+    if ( num > this.length ) return "Doesn't Exist!"
 
     //iterate through the nodes until count === num, and return that one
-    while(count < num){
+    while ( count < num ) {
       nodeToCheck = nodeToCheck.next;
       count++;
     }
     return nodeToCheck;
   }
 
-  remove(num){
+  remove (num) {
     let nodeToCheck = this.head,
       count = 0,
       prevNode = null
 
-    if(num > this.length) return "Doesn't Exist!"
+    if( num > this.length ) return "Doesn't Exist!";
 
-    if(num===0){
+    if( num === 0 ) {
       //if you want to remove the first node, aka the head
       this.head = nodeToCheck.next;
       this.length--;
       return this.head;
     }
 
-    while(count<num){
+    while( count < num ) {
       //iterate through the nodes until you get to the node at num
       prevNode = nodeToCheck;
       nodeToCheck = nodeToCheck.next;
@@ -91,3 +91,5 @@ cafes.add('Cafe Loft');
 cafes.add('Yellow House Cafe');
 cafes.add('Go Get Em Tiger');
 cafes.add('Bricks And Scones');
+console.log(cafes);
+console.log(cafes.get(2))
